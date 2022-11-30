e996dce89ef47e2f45329ede21afcd76c0803adc
s21166-pj <s21166@pjwstk.edu.pl>
2022-11-30 23:40:10 +0100

    Task 4 completed before documentation

---
+++ banknote_decision_trees.py
@@ -0,0 +1,96 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import genfromtxt
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+"""
+Authors:
+- Badysiak Pawel - s21166
+- Turek Wojciech - s21611
+"""
+
+def visualize_classifier(classifier, X, y, title=''):
+    # Define the minimum and maximum values for X and Y
+    # that will be used in the mesh grid
+    min_x, max_x = X[:, 0].min() - 1.0, X[:, 0].max() + 1.0
+    min_y, max_y = X[:, 1].min() - 1.0, X[:, 1].max() + 1.0
+
+    # Define the step size to use in plotting the mesh grid 
+    mesh_step_size = 0.01
+
+    # Define the mesh grid of X and Y values
+    x_vals, y_vals = np.meshgrid(np.arange(min_x, max_x, mesh_step_size), np.arange(min_y, max_y, mesh_step_size))
+
+    # Run the classifier on the mesh grid
+    output = classifier.predict(np.c_[x_vals.ravel(), y_vals.ravel()])
+
+    # Reshape the output array
+    output = output.reshape(x_vals.shape)
+
+    # Create a plot
+    plt.figure()
+
+    # Specify the title
+    plt.title(title)
+
+    # Choose a color scheme for the plot 
+    plt.pcolormesh(x_vals, y_vals, output, cmap=plt.cm.gray)
+
+    # Overlay the training points on the plot 
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=75, edgecolors='black', linewidth=1, cmap=plt.cm.Paired)
+
+    # Specify the boundaries of the plot
+    plt.xlim(x_vals.min(), x_vals.max())
+    plt.ylim(y_vals.min(), y_vals.max())
+
+    # Specify the ticks on the X and Y axes
+    plt.xticks((np.arange(int(X[:, 0].min() - 1), int(X[:, 0].max() + 1), 1.0)))
+    plt.yticks((np.arange(int(X[:, 1].min() - 1), int(X[:, 1].max() + 1), 1.0)))
+
+    plt.show()
+
+
+data = genfromtxt('data_banknote_authentication.csv', delimiter=',', skip_header=1)
+X, y = data[:, :2], data[:, -1]
+
+# Separate input data into two classes based on labels
+class_0 = np.array(X[y==0])
+class_1 = np.array(X[y==1])
+
+
+# Visualize input data
+plt.figure()
+plt.scatter(class_0[:, 0], class_0[:, 1], s=75, facecolors='black', 
+        edgecolors='black', linewidth=1, marker='x')
+plt.scatter(class_1[:, 0], class_1[:, 1], s=75, facecolors='white', 
+        edgecolors='black', linewidth=1, marker='o')
+plt.title('Input data')
+
+# Split data into training and testing datasets 
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=5)
+
+# Decision Trees classifier 
+params = {'random_state': 0, 'max_depth': 8}
+classifier = DecisionTreeClassifier(**params)
+classifier.fit(X_train, y_train)
+visualize_classifier(classifier, X_train, y_train, 'Training dataset')
+
+y_test_pred = classifier.predict(X_test)
+visualize_classifier(classifier, X_test, y_test, 'Test dataset')
+
+# Evaluate classifier performance
+class_names = ['Class-0', 'Class-1']
+print("\n" + "#"*40)
+print("\nClassifier performance on training dataset\n")
+print(classification_report(y_train, classifier.predict(X_train), target_names=class_names))
+print("#"*40 + "\n")
+
+print("#"*40)
+print("\nClassifier performance on test dataset\n")
+print(classification_report(y_test, y_test_pred, target_names=class_names))
+print("#"*40 + "\n")
+
+plt.show()
---
+++ svm.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from mlxtend.plotting import plot_decision_regions
+from sklearn import svm, model_selection, decomposition, metrics
+
+"""
+Authors:
+- Badysiak Pawel - s21166
+- Turek Wojciech - s21611
+Implementation of Decision Tree and Support Vector Machine
+There are two cases: 
+    a) Banknote Authenticity - check if banknote is authentic
+        Link to dataset( number 5 on the list): https://machinelearningmastery.com/standard-machine-learning-datasets/       
+    b) Phoneme - check if phoneme is nasal (Class 0) or oral (Class 1)
+        Link to dataset: https://github.com/jbrownlee/Datasets/blob/master/phoneme.csv
+        Link to description: https://github.com/jbrownlee/Datasets/blob/master/phoneme.names
+        
+    Install before:
+        pip install wheel
+        pip install pandas
+        pip install mlxtend
+        pip install numpy
+"""
+
+
+def classify_svc(dataset, split_size, kernel, C, gamma, dataset_name):
+
+    print(dataset.head(5))
+
+    x, y = dataset.drop('class', axis=1), dataset['class']
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(x, np.array(y), test_size=split_size)
+
+
+    svc = svm.SVC(kernel=kernel, C=C, gamma=gamma)
+    svc.fit(X_train, y_train)
+
+
+    y_model_result = svc.predict(X_test)
+    print(f"Sample results for {dataset_name}")
+    print(f"Model: {y_model_result[0:20]}")
+    print(f"Target: {y_test[0:20]}")
+
+
+    pca = decomposition.PCA(n_components=2)
+    X_train2 = pca.fit_transform(X_train)
+    svc.fit(X_train2, y_train)
+    plot_decision_regions(X_train2, y_train, clf=svc, legend=2)
+    plt.title(dataset_name)
+    plt.show()
+
+    score = metrics.accuracy_score(y_test, y_model_result)
+    return score
+
+
+if __name__ == "__main__":
+    # Load files
+    banknote = pd.read_csv("data_banknote_authentication.csv")
+    phoneme = pd.read_csv("phoneme.csv")
+
+    banknote_svc_result = classify_svc(banknote, 0.2, 'rbf', 10, 0.00001, 'Banknote Authenticity')
+    print(f"Precision of SVC model:\nBanknote dataset: {banknote_svc_result}")
+    phoneme_svc_result = classify_svc(phoneme, 0.2, 'rbf', 10000, 0.1, 'phoneme')
+    print(f"Precision of SVC model:\nphoneme dataset: {phoneme_svc_result}")
